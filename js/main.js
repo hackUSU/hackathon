bfe13e0bb7e4389bd28fff7b00f3dd6935492dab
Yancy Knight <yancy.knight@gmail.com>
2017-09-12 21:38:07 -0600

    #8 make navbar scroll, #14 fix carousel

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 require('../css/main.scss');
 $(document).ready(() => {
-  // debugger;
-  $('.carousel').slick()
+
+  // Setup slideshow
+  $(".slideshow").backstretch([
+    "../assets/logan1.jpg",
+    "../assets/logan2.jpg",
+    "../assets/logan3.jpg",
+    "../assets/logan4.jpg",
+  ], {fade: 750, duration: 2500});
+
+  // Make links smooth scroll to elements
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    var navbarHeight = 70;
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top - navbarHeight
+        }, 1000);
+    }
+});
+
 })
