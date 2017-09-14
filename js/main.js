1b5dd485c11cd9cf3ce714e22bd574bf5c4adbe6
Yancy Knight <yancy.knight@gmail.com>
2017-09-13 21:46:05 -0600

    #5 Change slideshow pictures.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,27 @@
 require('../css/main.scss');
 $(document).ready(() => {
 
-  // Setup slideshow
-  $(".slideshow").backstretch([
-    "assets/logan1.jpg",
-    "assets/logan2.jpg",
-    "assets/logan3.jpg",
-    "assets/logan4.jpg",
-  ], {fade: 750, duration: 2500});
+    // Setup slideshow
+    $(".slideshow").backstretch([
+        "assets/logan6.jpg",
+        "assets/logan10.jpg",
+        "assets/logan9.png",
+        "assets/logan7.jpg",
+        "assets/logan4.jpg",
+    ], {fade: 750, duration: 3000});
 
-  // Make links smooth scroll to elements
-  $('a[href^="#"]').on('click', function(event) {
-    var href = this.getAttribute('href');
-    console.log(href.indexOf('faq'))
-    var target = $(href);
-    var navbarHeight = 70;
-    if( target.length && href.indexOf('collapse') == -1) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top - navbarHeight
-        }, 1000);
-    }
-});
+    // Make links smooth scroll to elements
+    $('a[href^="#"]').on('click', function(event) {
+        var href = this.getAttribute('href');
+        console.log(href.indexOf('faq'))
+        var target = $(href);
+        var navbarHeight = 70;
+        if( target.length && href.indexOf('collapse') == -1) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top - navbarHeight
+            }, 1000);
+        }
+    });
 
 })
