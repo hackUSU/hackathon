4cdae46d89b385c06864908682fd0239e350acab
Yancy Knight <yancy.knight@gmail.com>
2017-09-18 21:25:54 -0600

    Change build to production mode.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 require('../css/main.scss');
 require('./jquery.backstretch.min');
-$(document).ready(() => {
+$(document).ready(function() {
 
     // Setup slideshow
     $(".slideshow").backstretch([
